d7165c7b91d5c42efce5c7bd188511ca4f55068c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-10 15:04:53 -0800

    learn how to assign types to functions and its parameters

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,15 @@
 // let a  = 67
 // a = "talha"  // cannot assign a number type to string type in TS
-/////////////// asigning types
+/////////////// asigning types to variables
 let myName:string = "Talha" // by this way we can added types to a variable
 myName = "Talha Nawaz"
 let myNum : number = 45
 myNum = 45
 ////////////////////////////////
+///////// asigning types to functions
+const sum  = (a:number ,b:number)=>{ // by this way we can give types to functions parameters
+    return a +b
+}
+const sum2 = (a:number,b:number):number => { // by this way we can assign types to functions
+    return a+b
+}
