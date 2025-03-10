bf7194e27fc559d88f53b369bff4036f9fd09e76
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-10 14:48:47 -0800

    learn how to assign types to variables

---
+++ js/index.js
@@ -0,0 +1,2 @@
+let nam = "talha"
+nam = 89
---
+++ ts/index.ts
@@ -0,0 +1,8 @@
+// let a  = 67
+// a = "talha"  // cannot assign a number type to string type in TS
+/////////////// asigning types
+let myName:string = "Talha" // by this way we can added types to a variable
+myName = "Talha Nawaz"
+let myNum : number = 45
+myNum = 45
+////////////////////////////////
