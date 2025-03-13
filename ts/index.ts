784ece34c76cd268524d0ce256c90caa122d8a86
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-13 15:06:36 -0800

    learn about noEmitOnError in tsconfig.json file

--- ts/index.ts
+++ ts/index.ts
@@ -71,8 +71,11 @@
 // let num2:number =20
 // let sum:number = num1+num2
 // console.log(sum)
-const sum =(num1:number,num2:number):number =>{ // made a function of sum two numbers
-  return num1+num2
-}
-let check = sum(74,3)
-console.log(check)
+// const sum =(num1:number,num2:number):number =>{ // made a function of sum two numbers
+//   return num1+num2
+// }
+// let check = sum(74,3)
+// console.log(check)
+let num:number=67
+let num2:number = "ks"
+console.log(num+num2)
