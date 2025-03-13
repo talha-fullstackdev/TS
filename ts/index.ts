80dbc669c26e206dfb5a563002550be83bbbc34b
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-13 12:10:00 -0800

    made two types and extend one from another and made a object by using properties from both classes

--- ts/index.ts
+++ ts/index.ts
@@ -17,22 +17,35 @@
 //   return a + b;
 // };
 // let num:number =34
-// num = "34" 
+// num = "34"
 // let names:string[]=[]
 // names.push("7")
 // console.log(names)
 // let myName :{name:string} // here we initially made an object and assign its property types only here we say object have only one property of type string
 // myName = {name:"Talha Nawaz"}
-// let myName :{name:string,age?:number} // here we made age optional 
+// let myName :{name:string,age?:number} // here we made age optional
 // myName = {name:"Talha Nawaz",age:34}
-interface Person{ // made interface like we made classes and objects in javascript
-    name:string;
-    age:number;
-    gender:string;
-}
-let talha :Person
-talha = {
-    name:"Talha nawaz",
-    age:24,
-    gender:"male"
+// interface Person {
+//   // made interface like we made classes and objects in javascript
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+// let talha: Person;
+// talha = {
+//   name: "Talha nawaz",
+//   age: 24,
+//   gender: "male",
+// };
+type Animal = { // made a animal type
+  name: string;
+  isPet: boolean;
+};
+type Cow = Animal & { //made a cow type and extend it from animal type so it can use animal types as well
+  isVeg: boolean
+};
+const pet1: Cow = { // made a pet1 object which uses both animal types and cow types
+  name:"horse",
+  isPet:true,
+  isVeg:true
 }
