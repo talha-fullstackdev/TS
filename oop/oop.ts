b96c95584c2e027cb9544a7942e80b5308c35189
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-26 15:01:32 +0500

    updated

--- oop/oop.ts
+++ oop/oop.ts
@@ -50,39 +50,53 @@
 /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ///////// Inheritance in TypeScript
-class Person {
-    name : string;
-    age : number;
-    protected rollNum : number;
-    hobbies : string[];
-    constructor(name: string, age: number, rollNum: number, hobbies: string[]) {
-      this.name = name;
-      this.age = age;
-      this.rollNum = rollNum;
-      this.hobbies = hobbies;
-    }
-    getPersonDetails(){ // made method/function in a class
-      console.log(`student name is ${this.name} and his rollnumber is ${this.rollNum}`)
-    }
-  }
-class Employee extends Person { // this way we can implement inheritance 
-  emp_id : number; // separete properties of child class
-  department : string;
-  constructor(name: string, age: number, rollNum: number, hobbies: string[],emp_id:number,department:string){ // we can also provide parent class properties here in child class constructor
-    super(name,age,rollNum,hobbies) // we provide properties using from parent class like this in super method (basically it is used to called the parent class constructor)
-    this.emp_id = emp_id
-    this.department = department
-  }
-  getEmployeeDetails(){
-    console.log(`emp name is ${this.name} his roll number is ${this.rollNum} hobbies ${this.hobbies} emp-id ${this.emp_id} and department is ${this.department}`)
-  }
-}  
-const emp1 : Employee = new Employee("talha", 24, 213, ["coding"], 234, "dev")
-// console.log(emp1.rollNum) // cannot get it here because it is protected and used in parent and its child classes only
-console.log(emp1)
+// class Person {
+//     name : string;
+//     age : number;
+//     protected rollNum : number;
+//     hobbies : string[];
+//     constructor(name: string, age: number, rollNum: number, hobbies: string[]) {
+//       this.name = name;
+//       this.age = age;
+//       this.rollNum = rollNum;
+//       this.hobbies = hobbies;
+//     }
+//     getPersonDetails(){ // made method/function in a class
+//       console.log(`student name is ${this.name} and his rollnumber is ${this.rollNum}`)
+//     }
+//   }
+// class Employee extends Person { // this way we can implement inheritance 
+//   emp_id : number; // separete properties of child class
+//   department : string;
+//   constructor(name: string, age: number, rollNum: number, hobbies: string[],emp_id:number,department:string){ // we can also provide parent class properties here in child class constructor
+//     super(name,age,rollNum,hobbies) // we provide properties using from parent class like this in super method (basically it is used to called the parent class constructor)
+//     this.emp_id = emp_id
+//     this.department = department
+//   }
+//   getEmployeeDetails(){
+//     console.log(`emp name is ${this.name} his roll number is ${this.rollNum} hobbies ${this.hobbies} emp-id ${this.emp_id} and department is ${this.department}`)
+//   }
+// }  
+// const emp1 : Employee = new Employee("talha", 24, 213, ["coding"], 234, "dev")
+// // console.log(emp1.rollNum) // cannot get it here because it is protected and used in parent and its child classes only
+// console.log(emp1)
 ////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 ////////// Public, Protected & Private 🔒 Access Modifiers in Object-Oriented Programming in TypeScript
 // global >>> by default accessable in every where
 // protected >>> only accessable in parent class and child class but not outside the class
 // private  >>> only accessable in class where it is defined and cannnot used in child classes or outside the class
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+class Employee{  // we can also mad constructor like this without using property agian and agian like we did in upper classes  , here but we have to use access modifers to use this functionality ,this will also reduce our code
+    constructor(
+       public name:string,
+        public age:number,
+        public gender:string
+    ){}
+    intro(){
+        console.log(`emp name is ${this.name} his age is ${this.age} and his gender is ${this.gender}`)
+    }
+}
+const emp1 :Employee = new Employee("talha",24,"male")
+console.log(emp1)
