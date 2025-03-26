5f2f27c6b6c7418c2783e34b9b04d2500a37c01a
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-26 11:57:29 +0500

    learn how to make constructor in TS

--- oop/oop.js
+++ oop/oop.js
@@ -1,13 +1,41 @@
 "use strict";
-class Persons {
-    constructor() {
-        // this way we make  class in typescript
-        this.name = "Talha";
-        this.age = 24;
-        this.hobbies = ["coding", "reading", "watching documentries"];
+// class Persons {
+//   // this way we make  class in typescript
+//   name: string = "Talha";
+//   age: number = 24;
+//   hobbies: string[] = ["coding", "reading", "watching documentries"];
+// }
+// const std1: Persons = new Persons(); // this way we create a object/instance of the class
+// console.log(std1)
+// console.log(std1.name);
+// console.log(std1.age);
+// console.log(std1.hobbies);
+/////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+/// made constructor in class
+class Person {
+    constructor(name, age, rollNum, hobbies) {
+        this.name = name;
+        this.age = age;
+        this.rollNum = rollNum;
+        this.hobbies = hobbies;
     }
 }
-const std1 = new Persons();
-console.log(std1.name);
-console.log(std1.age);
-console.log(std1.hobbies);
+const st1 = new Person("Talha", 24, 213, [
+    "coding",
+    "reading",
+    "pet foastring",
+]);
+const st2 = new Person("hamza", 23, 123618, [
+    "snooker",
+    "travling",
+    "reading",
+]);
+const st3 = new Person("Talha", 24, 213, [
+    "vollyball",
+    "swimming",
+    "cricket",
+]);
+console.log(st1);
+console.log(st2);
+console.log(st3);
--- oop/oop.ts
+++ oop/oop.ts
@@ -1,10 +1,45 @@
-class Persons {
-  // this way we make  class in typescript
-  name: string = "Talha";
-  age: number = 24;
-  hobbies: string[] = ["coding", "reading", "watching documentries"];
+// class Persons {
+//   // this way we make  class in typescript
+//   name: string = "Talha";
+//   age: number = 24;
+//   hobbies: string[] = ["coding", "reading", "watching documentries"];
+// }
+// const std1: Persons = new Persons(); // this way we create a object/instance of the class
+// console.log(std1)
+// console.log(std1.name);
+// console.log(std1.age);
+// console.log(std1.hobbies);
+/////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+/// made constructor in class
+class Person {
+  name: string;
+  age: number;
+  rollNum: number;
+  hobbies: string[];
+
+  constructor(name: string, age: number, rollNum: number, hobbies: string[]) {
+    this.name = name;
+    this.age = age;
+    this.rollNum = rollNum;
+    this.hobbies = hobbies;
+  }
 }
-const std1: Persons = new Persons(); // this way we create a object/instance of the class
-console.log(std1.name);
-console.log(std1.age);
-console.log(std1.hobbies);
+const st1: Person = new Person("Talha", 24, 213, [
+  "coding",
+  "reading",
+  "pet foastring",
+]);
+const st2: Person = new Person("hamza", 23, 123618, [
+  "snooker",
+  "travling",
+  "reading",
+]);
+const st3: Person = new Person("Talha", 24, 213, [
+  "vollyball",
+  "swimming",
+  "cricket",
+]);
+console.log(st1);
+console.log(st2);
+console.log(st3);
