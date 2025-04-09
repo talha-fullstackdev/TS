c291649fb075f1a63bd483a47c20592aab2c1b42
talha-fullstackdev <talhafullstack786@gmail.com>
2025-04-09 15:58:20 +0500

    updated

--- ts/index.js
+++ ts/index.js
@@ -102,11 +102,35 @@ const item2 = {
     price: 120000,
     quantity: 500,
 };
+const item3 = {
+    name: "mobile",
+    price: 12000,
+    quantity: 700,
+};
+const item4 = {
+    name: "keyboard",
+    price: 1200,
+    quantity: 600,
+};
 const calPrice = (product) => {
     const { name, price, quantity } = product;
     const totalPrice = price * quantity;
-    const discountPrice = name == "Headphones" ? totalPrice * 0.05 : totalPrice * 0.001;
+    let discountPrice = 0;
+    if (name == "Headphones") {
+        discountPrice = totalPrice * 0.05;
+    }
+    if (name == "laptop") {
+        discountPrice = totalPrice * 0.001;
+    }
+    if (name == "mobile") {
+        discountPrice = totalPrice * 0.02;
+    }
+    if (name == "keyboard") {
+        discountPrice = totalPrice * 0.01;
+    }
+    //   const discountPrice =
+    //     name == "Headphones" ? totalPrice * 0.05 : totalPrice * 0.001;
     return `the name of product is ${name}\n and quantity is ${quantity}\n and price per unit is ${price}\n and total price will be ${totalPrice - discountPrice} after discount \n and discount given is ${discountPrice}`;
 };
-const checkDetails = calPrice(item1);
+const checkDetails = calPrice(item4);
 console.log(checkDetails);
